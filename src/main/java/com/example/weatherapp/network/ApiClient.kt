2f92820ddf26307e22f96701610c2dd57edfc189
Kaszioq <kubakaszewskisl@gmail.com>
2025-06-01 00:03:33 +0200

    Fix

--- src/main/java/com/example/weatherapp/network/ApiClient.kt
+++ src/main/java/com/example/weatherapp/network/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "b8c57cd19b6b6a57c3b0c9a2fa3521fa"
+    const val API_KEY = ""
 
         private val retrofit by lazy {
         val logging = HttpLoggingInterceptor().apply {
